90c38eb14128612e93d159cddd5c21b4efcdad33
Adam Langsner <adamlangsner@gmail.com>
2015-04-12 10:36:09 -0400

    test for util.guid()

--- test/francis.spec.js
+++ test/francis.spec.js
@@ -64,4 +64,10 @@ describe('Francis', function() {
             expect(registerLogger(logger)).to.not.throw();
         });
     });
+
+    describe('util', function() {
+        describe('#guid()', function() {
+            expect(Francis.util.guid()).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-(8|9|a|b)[0-9a-f]{3}-[0-9a-f]{12}$/);
+        });
+    });
 });
